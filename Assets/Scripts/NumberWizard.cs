7729b73c871a8b1d75e0b19b32187d5974b36e6e
jd4rider <jd4rider@gmail.com>
2019-12-08 14:57:49 -0700

    final

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -9,8 +9,11 @@ public class NumberWizard : MonoBehaviour
     [SerializeField] int max;
     [SerializeField] int min;
     [SerializeField] TextMeshProUGUI guessText;
+    [SerializeField] TextMeshProUGUI thinkingText;
 
     int guess;
+    ArrayList alreadyGuessed = new ArrayList();
+    bool isPicked = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +23,8 @@ public class NumberWizard : MonoBehaviour
 
     void StartGame()
     {
-        guess = (max + min) / 2;
-        guessText.text = guess.ToString();
-        max = max + 1;
+        thinkingText.text = "The number you are thinking of is:";
+        NextGuess();
     }
 
     public void OnPressHigher()
@@ -39,7 +41,26 @@ public class NumberWizard : MonoBehaviour
 
     private void NextGuess()
     {
-        guess = (max + min) / 2;
-        guessText.text = guess.ToString();
+        guess = Random.Range(min, max);
+
+        if (max - min > 1)
+        {
+            if (alreadyGuessed.Contains(guess))
+            {
+                NextGuess();
+            }
+            alreadyGuessed.Add(guess);
+            guessText.text = guess.ToString();
+        }
+        else if (isPicked)
+        {
+            thinkingText.text = "Now you are just messing with me, there is no other possible answer:";
+        }
+        else
+        {
+            if (guess >= 999 && guess < 1000) guess = 1000;
+            isPicked = true;
+            guessText.text = guess.ToString();
+        }
     }
 }
